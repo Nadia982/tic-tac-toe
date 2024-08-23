daa95f5888e195beebf15f0ce83f371287a06f01
Natalie Gillam <ngillam@hotmail.co.uk>
2024-08-23 22:02:01 +0100

    added function to handle clicking edit button next to player names

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import { useState } from "react";
 
-const Player = ({name, symbol}) => {
+const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+ 
+  const handleEditClick = () => {
+    setIsEditing(!isEditing);
+  };
+
+  let playerName = isEditing ? <input type="text" required/> : <span className="player-name">{name}</span>
+   
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 };
